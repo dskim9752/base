5d66642f9c320686cd6dfed6c98d96aa5d8b5658
David96 <xd_e-mail@gmx.de>
2014-01-31 19:05:36 +0100

    Lockscreen Notifications: dismiss all button, improve animations, cleanup
    Change-Id: I408b69a39d9551d00ebc8aa54fb4ef7377fe818b

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
@@ -982,7 +982,7 @@ public class KeyguardViewManager {
     }
 
     public void dispatchButtonClick(int buttonId) {
-        mNotificationView.showAllNotifications();
+        mNotificationView.onButtonClick(buttonId);
     }
 
     public void launchCamera() {
