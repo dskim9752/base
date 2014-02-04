ac8839fb96663e4d5cc88e28402f01c360ceddbc
Jinpyo-laptop <neighbors28@naver.com>
2014-02-04 22:28:20 +0900

    Revert setWifiEnabled code

--- wifi/java/android/net/wifi/WifiManager.java
+++ wifi/java/android/net/wifi/WifiManager.java
@@ -1055,7 +1055,7 @@ public class WifiManager {
             AppOpsManager.MODE_ALLOWED)
             return false;
         try {
-            return mService.setWifiEnabled(mContext.getBasePackageName(), enabled);
+            return mService.setWifiEnabled(enabled);
         } catch (RemoteException e) {
             return false;
         }
