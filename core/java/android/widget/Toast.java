PAC-ROM/android_frameworks_base

Toast Animation[2/2]
Change-Id: Ib6b5aa94237207ada78c512997c8fc9765d68d1f

Conflicts:
	core/java/android/provider/Settings.java

Change-Id: Ib6b5aa94237207ada78c512997c8fc9765d68d1f

core/java/android/widget/Toast.java
@@ -1,5 +1,7 @@
 /*
  * Copyright (C) 2007 The Android Open Source Project
+ * This code has been modified. Portions copyright (C) 2013-2014, Dokdo Project - Gwon Hyeok
+ * Thx to zst123 (https://github.com/zst123/XuiMod)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,6 +28,7 @@ import android.os.Handler;
 import android.os.Message;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -393,6 +396,56 @@ public class Toast {
                 // the layout direction
                 final Configuration config = mView.getContext().getResources().getConfiguration();
                 final int gravity = Gravity.getAbsoluteGravity(mGravity, config.getLayoutDirection());
+                switch(Settings.System.getInt(context.getContentResolver(), Settings.System.TOAST_ANIMATION, 1)) {
+                case 0:
+                mParams.windowAnimations = -1;
+                break;
+                case 1:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast;
+                break;
+                case 2:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_Fade;
+                break;
+                case 3:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_SlideRight;
+                break;
+                case 4:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_SlideLeft;
+                break;
+                case 5:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_Xylon;
+                break;
+                case 6:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_Toko;
+                break;
+                case 7:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_Tn;
+                break;
+                case 8:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_Honami;
+                break;
+                case 9:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_FastFade;
+                break;
+                case 10:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_GrowFade;
+                break;
+                case 11:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_GrowFadeCenter;
+                break;
+                case 12:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_GrowFadeBottom;
+                break;
+                case 13:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_Translucent;
+                break;
+                case 14:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_SlideLeftRight;
+                break;
+                case 15:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_SlideRightLeft;
+                break;
+                 }
                 mParams.gravity = gravity;
                 if ((gravity & Gravity.HORIZONTAL_GRAVITY_MASK) == Gravity.FILL_HORIZONTAL) {
                     mParams.horizontalWeight = 1.0f;

dskim9752/base

core/java/android/widget/Toast.java
@@ -1,5 +1,7 @@
 /*
  * Copyright (C) 2007 The Android Open Source Project
+ * This code has been modified. Portions copyright (C) 2013-2014, Dokdo Project - Gwon Hyeok
+ * Thx to zst123 (https://github.com/zst123/XuiMod)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,6 +27,7 @@ import android.graphics.PixelFormat;
 import android.os.Handler;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -344,7 +347,7 @@ public class Toast {
             params.height = WindowManager.LayoutParams.WRAP_CONTENT;
             params.width = WindowManager.LayoutParams.WRAP_CONTENT;
             params.format = PixelFormat.TRANSLUCENT;
-            params.windowAnimations = com.android.internal.R.style.Animation_Toast;
+	    params.windowAnimations = com.android.internal.R.style.Animation_Toast;
             params.type = WindowManager.LayoutParams.TYPE_TOAST;
             params.setTitle("Toast");
             params.flags = WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
@@ -386,6 +389,50 @@ public class Toast {
                 // the layout direction
                 final Configuration config = mView.getContext().getResources().getConfiguration();
                 final int gravity = Gravity.getAbsoluteGravity(mGravity, config.getLayoutDirection());
+            switch(Settings.System.getInt(context.getContentResolver(), Settings.System.TOAST_ANIMATION, 1)) {
+                case 0:
+                mParams.windowAnimations = -1;
+                break;
+                case 1:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast;
+                break;
+                case 2:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_Fade;
+                break;
+                case 3:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_SlideRight;
+                break;
+                case 4:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_SlideLeft;
+                break;
+                case 5:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_Xylon;
+                break;
+                case 6:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_Toko;
+                break;
+                case 7:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_Tn;
+                break;
+                case 8:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_Honami;
+                break;
+                case 9:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_FastFade;
+                break;
+                case 10:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_GrowFade;
+                break;
+                case 11:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_GrowFadeCenter;
+                break;
+                case 12:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_GrowFadeBottom;
+                break;
+                case 13:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_Translucent;
+                break;
+                }
                 mParams.gravity = gravity;
                 if ((gravity & Gravity.HORIZONTAL_GRAVITY_MASK) == Gravity.FILL_HORIZONTAL) {
                     mParams.horizontalWeight = 1.0f;